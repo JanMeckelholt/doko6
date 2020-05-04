962f22d17baf1e2435a26c1898ef56d6b250dab9
JanMeckelholt <Jan_Meckelholt@web.de>
2020-05-04 10:21:24 +0200

    model hand - cards on delete cascade

--- db/migrate/20200503130040_add_hand_to_card.rb
+++ db/migrate/20200503130040_add_hand_to_card.rb
@@ -1,5 +1,5 @@
 class AddHandToCard < ActiveRecord::Migration[6.0]
   def change
-    add_reference :cards, :hand, foreign_key: true
+    add_reference :cards, :hand, foreign_key: true, on_delete: :cascade
   end
 end
