40fb278e0c0ca64d47b1f3bb22b7179d0d95184b
JanMeckelholt <Jan_Meckelholt@web.de>
2020-05-10 21:24:19 +0200

    migrations

--- db/migrate/20200503131256_add_game_to_game_player.rb
+++ db/migrate/20200503131256_add_game_to_game_player.rb
@@ -1,6 +1,6 @@
 class AddGameToGamePlayer < ActiveRecord::Migration[6.0]
   def change
-    remove_column :game_players, :game
+
     remove_column :game_players, :player
     add_reference :game_players, :game, foreign_key: true
   end
