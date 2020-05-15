f1fbe920d4038c95c77817d6af343a8735589276
JanMeckelholt <Jan_Meckelholt@web.de>
2020-05-15 08:39:34 +0200

    order by card_id in hand

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -2,7 +2,7 @@ class Hand < ApplicationRecord
 
   #include ActiveModel::Model
   #
-  has_many :cards, -> { order(card_id: :desc) }  
+  has_many :cards, -> { order(id: :desc) }  
   belongs_to :game_player, optional: true
  
   validates :cards, length:{maximum: 10}
